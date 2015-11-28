7a6e9c56c83728fc93f9773f52d1f145778319b4
Svetoslav Nedkov <sveto.n@gmail.com>
2015-11-28 23:01:51 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import httpProxy from 'http-proxy';
+
+if (!process.env.LOCAL_URL) {
+    console.log('Please supply the LOCAL_URL environment variable.');
+    process.exit(1);
+}
+
+var proxy = httpProxy.createProxyServer({
+    target: process.env.LOCAL_URL
+}).listen(process.env.PORT);
+
+proxy.on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+console.log(`Proxy listening on port ${process.env.PORT} forwarding traffic to: ${process.env.LOCAL_URL}`);
